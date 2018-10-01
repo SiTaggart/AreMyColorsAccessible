afbf29f44c84c2168232691200c013acbface314
Si Taggart <me@simontaggart.com>
2018-09-30 19:01:34 -0700

    chore(eslint) lint

--- components/layouts/layout-shared/__tests__/index.spec.js
+++ components/layouts/layout-shared/__tests__/index.spec.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import LayoutShared from '../index';
 
 describe('Layout Shared', () => {
