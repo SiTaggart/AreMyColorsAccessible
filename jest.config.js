b5d6400750de1887471f1a414d416a47510182ef
Si Taggart <me@simontaggart.com>
2019-04-07 15:33:31 -0700

    chore(jest): remove deprecation warning

--- jest.config.js
+++ jest.config.js
@@ -113,7 +113,7 @@ module.exports = {
   // setupFiles: [],
 
   // The path to a module that runs some code to configure or set up the testing framework before each test
-  setupTestFrameworkScriptFile: './jest.setup.js',
+  setupFilesAfterEnv: ['./jest.setup.js'],
 
   // A list of paths to snapshot serializer modules Jest should use for snapshot testing
   // snapshotSerializers: [],
