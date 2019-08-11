79b7607342ca103260795e1c8667363a3dec048f
Si Taggart <me@simontaggart.com>
2019-08-11 14:48:25 -0700

    chore(percy): Add some VRT via Percy.io

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -7,11 +7,13 @@
 // You can read more here:
 // https://on.cypress.io/plugins-guide
 // ***********************************************************
+const percyHealthCheck = require('@percy/cypress/task');
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-module.exports = () => {
+module.exports = on => {
+  on('task', percyHealthCheck);
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 };
