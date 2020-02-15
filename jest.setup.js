77b74f2c47a72edd35a6e70ff0c8401831fd4b5a
Si Taggart <me@simontaggart.com>
2020-02-15 11:14:03 -0800

    fix: lint

--- jest.setup.js
+++ jest.setup.js
@@ -3,6 +3,7 @@ const Enzyme = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
 
 // Needed for Enzyme
+// eslint-disable-next-line @typescript-eslint/no-implied-eval
 global.requestAnimationFrame = cb => setTimeout(cb, 0);
 
 Enzyme.configure({ adapter: new Adapter() });
