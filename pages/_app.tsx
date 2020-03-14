74c505d8cb689f607b0b94ffb0f395260bbb9c91
Si Taggart <me@simontaggart.com>
2020-03-14 11:22:12 -0700

    chore: update bundlesize and optimise build

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,33 +1,13 @@
-import React, { ReactElement } from 'react';
-import App, { AppContext } from 'next/app';
+import React from 'react';
+import { AppProps } from 'next/app';
 import { Global } from '@emotion/core';
 import { globalStyles } from '../styles/global';
 
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => (
+  <>
+    <Global styles={globalStyles} />
+    <Component {...pageProps} />
+  </>
+);
 // eslint-disable-next-line import/no-default-export
-export default class MyApp extends App {
-  public static async getInitialProps({
-    Component,
-    ctx,
-  }: AppContext): Promise<{
-    pageProps: {};
-  }> {
-    let pageProps = {};
-
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
-    }
-
-    return { pageProps };
-  }
-
-  public render(): ReactElement<HTMLDivElement> {
-    const { Component, pageProps } = this.props;
-
-    return (
-      <>
-        <Global styles={globalStyles} />
-        <Component {...pageProps} />
-      </>
-    );
-  }
-}
+export default MyApp;
