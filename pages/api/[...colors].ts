d9e69f6077231348fcec8b0a7c707109880c13a8
Si Taggart <me@simontaggart.com>
2021-03-28 18:15:10 -0700

    chore: better logs

--- pages/api/[...colors].ts
+++ pages/api/[...colors].ts
@@ -16,21 +16,21 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   logger.info('url', { url: req.url });
 
   const { colors } = req.query;
-  logger.info('query', req.query);
+  logger.info('query', { query: req.query });
   const colorsArray = ensureColorsAreAnArrayOfTwo(colors);
 
   if (colorsArray) {
-    logger.info('color array', colorsArray);
+    logger.info('color array', { colorsArray });
     let rating;
     try {
       rating = getRating(colorsArray);
     } catch (error) {
-      logger.error('get rating', error);
+      logger.error('get rating', { error });
     }
-    logger.info('rating', rating);
+    logger.info('rating', { rating });
     res.json(rating);
   } else {
-    logger.error('no array', req.query);
+    logger.error('no array', { query: req.query });
     res.status(500).json({ message: 'API Error: must send a colors key with array of two colors' });
   }
 }
