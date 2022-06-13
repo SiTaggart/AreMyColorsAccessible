a46170251f5609fdd890ccb7528f62f4a93c9c4e
Si Taggart <me@simontaggart.com>
2022-06-12 21:17:43 -0700

    chore: lint

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'cypress';
 
+// eslint-disable-next-line import/no-default-export
 export default defineConfig({
   projectId: 'yn654t',
   fixturesFolder: false,
@@ -7,7 +8,8 @@ export default defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config);
+      // eslint-disable-next-line global-require
+      return require('./cypress/plugins/index')(on, config);
     },
     baseUrl: 'http://localhost:3000',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
